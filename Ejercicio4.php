5e0abf46240f33c131f617e9c38a6652316840fd
Asdru Díaz <125107126+asdruds@users.noreply.github.com>
2023-09-27 18:16:40 -0600

    Ejercicio 4 con PHP
    Se piden unos datos para formar una oración, no entiendo mucho pero ahí ando.
    
    Además hay un Select para desplegar opciones

---
+++ Ejercicio4.php
@@ -0,0 +1,32 @@
+<?php
+    
+    if($_POST){
+
+        $Edad = $_POST['Edad'];
+        $Nombre = $_POST['Nombre'];
+        $Estatura = $_POST['Estatura'];
+        $EstadoCivil = $_POST['EstadoCivil'];
+
+        echo "El perfil de Nombre: " . $Nombre . ", con Edad: " . $Edad . " años, tiene una Estatura de " . $Estatura . " m, y es: " . $EstadoCivil . " .";
+
+    } else {
+
+        echo
+        '<form action="Ejercicio4.php" method="post">
+
+        Nombre: <input type="text" name="Nombre"><br>
+        Edad: <input type="number" name="Edad"><br>
+        Estatura: <input type="text" name="Estatura"><br>
+        Estado Civil:
+            <select name="EstadoCivil" >
+                <option value= "Casado">Casado</option>
+                <option value= "Soltero">Soltero</option>
+            </select>  
+        <input type="submit" value="Enviar">  
+    
+        </form>';
+
+    }
+
+?>
+
