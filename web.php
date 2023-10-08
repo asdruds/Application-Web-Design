75f22af84e02b4de52c0d8c69159c44443b983b1
Asdru Díaz <125107126+asdruds@users.noreply.github.com>
2023-10-08 00:28:07 -0600

    Update web.php

--- web.php
+++ web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('Welcome :)');
+    return ('Welcome :)');
 });
 
 //http://localhost/actividad7/public/courses
